3415e5b45bad79481b63a9f70dd5c92bcecd25d0
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 15:36:54 +0900

    feat: 기능 선택 입력 메소드 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,9 +1,22 @@
 package pairmatching.view;
 
 import pairmatching.enums.ViewMessage;
+import pairmatching.util.ValidateOption;
 import pairmatching.util.Validator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+    private static final ValidateOption validateOption = new ValidateOption();
+
+    public static String readOption() {
+        printMessage(ViewMessage.INPUT_OPTION);
+        String option = "";
+        while (option.length() == 0) {
+            option = getValid(readLine(), validateOption);
+        }
+        return option;
+    }
 
     private static String getValid(String input, Validator validator) {
         try {
