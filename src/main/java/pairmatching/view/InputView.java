0146543e00fe81c51003b1d6da8f27d9f2a54ae1
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 15:36:00 +0900

    feat: 입력값 유효성 검사 메소드 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,8 +1,19 @@
 package pairmatching.view;
 
 import pairmatching.enums.ViewMessage;
+import pairmatching.util.Validator;
 
 public class InputView {
+
+    private static String getValid(String input, Validator validator) {
+        try {
+            validator.validate(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return "";
+        }
+        return input;
+    }
 
     private static void printMessage(String message) {
         System.out.println(message);
