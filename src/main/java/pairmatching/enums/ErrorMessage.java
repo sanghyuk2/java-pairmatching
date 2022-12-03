e022826fe8120cd36de4f1a60f171fa24fbe7959
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 22:30:57 +0900

    feat: 과정 유효성 검사 시 예외 발생문 추가

--- src/main/java/pairmatching/enums/ErrorMessage.java
+++ src/main/java/pairmatching/enums/ErrorMessage.java
@@ -1,7 +1,9 @@
 package pairmatching.enums;
 
 public enum ErrorMessage {
-    INVALID_OPTION("기능 번호 중 하나를 입력하셔야 합니다.");
+    INVALID_OPTION("기능 번호 중 하나를 입력하셔야 합니다."),
+    INVALID_COURSE("백엔드, 프론트엔드 중 하나를 입력하셔야 합니다."),
+    INVALID_INPUT("잘못 입력하셨습니다.");
 
     private final String value;
     private static final String errorHead = "[ERROR] ";
