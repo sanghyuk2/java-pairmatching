2d036a974f35ca99672284caea665c4946614465
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 22:34:12 +0900

    feat: 레벨 유효성 검사 시 예외 발생할 경우 출력될 출력문 추가

--- src/main/java/pairmatching/enums/ErrorMessage.java
+++ src/main/java/pairmatching/enums/ErrorMessage.java
@@ -3,6 +3,7 @@ package pairmatching.enums;
 public enum ErrorMessage {
     INVALID_OPTION("기능 번호 중 하나를 입력하셔야 합니다."),
     INVALID_COURSE("백엔드, 프론트엔드 중 하나를 입력하셔야 합니다."),
+    INVALID_LEVEL("레벨1에서 레벨5 중 하나를 입력하셔야 합니다."),
     INVALID_INPUT("잘못 입력하셨습니다.");
 
     private final String value;
