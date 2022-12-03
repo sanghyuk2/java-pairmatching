64d00ad1a9fec0d2b2356372468dd075f65a6330
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 22:29:26 +0900

    feat: 입력값에 일치하는 과정을 반환하는 메소드 구현

--- src/main/java/pairmatching/enums/Course.java
+++ src/main/java/pairmatching/enums/Course.java
@@ -1,5 +1,7 @@
 package pairmatching.enums;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
@@ -8,6 +10,17 @@ public enum Course {
 
     Course(String name) {
         this.name = name;
+    }
+
+    public static Course findCourse(String course) {
+        return Arrays.stream(Course.values())
+                .filter(c -> c.isMatch(course))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_COURSE.getValue()));
+    }
+
+    private boolean isMatch(String course) {
+        return this.name.equals(course);
     }
 
     // 추가 기능 구현
