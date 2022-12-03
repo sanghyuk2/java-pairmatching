3d8b6fa604bc0f6b6e9866bba88ff1c75775b0dc
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 15:39:50 +0900

    feat: 기능 선택 메서드 틀 구현

--- src/main/java/pairmatching/domain/PairMatchingGame.java
+++ src/main/java/pairmatching/domain/PairMatchingGame.java
@@ -2,6 +2,10 @@ package pairmatching.domain;
 
 public class PairMatchingGame {
     public void start() {
+        while(selectOption());
+    }
 
+    public boolean selectOption() {
+        return true;
     }
 }
