d0ef5d15ff9e6e482be3bca2c431cd2ce041ece7
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 15:28:35 +0900

    feat: 기능 선택 안내문 추가 및 기본 틀 구현

--- src/main/java/pairmatching/enums/ViewMessage.java
+++ src/main/java/pairmatching/enums/ViewMessage.java
@@ -1,5 +1,19 @@
 package pairmatching.enums;
 
 public enum ViewMessage {
+    INPUT_OPTION("기능을 선택하세요.\n" +
+            Menu.MATCHING.getMenuNumber() + ". " + Menu.MATCHING.getMenuName() + "\n" +
+            Menu.SEARCH.getMenuNumber() + ". " + Menu.SEARCH.getMenuName() + "\n" +
+            Menu.INITIALIZE.getMenuNumber() + ". " + Menu.INITIALIZE.getMenuName() + "\n" +
+            Menu.QUIT.getMenuNumber() + ". " + Menu.QUIT.getMenuName());
 
+    private final String value;
+
+    ViewMessage(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
