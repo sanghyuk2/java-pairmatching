88e9e1a122686c65051b98b3fd9ade4450889ed2
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-03 15:23:13 +0900

    feat: 기능 선택지를 구성하는 enum 구현

---
+++ src/main/java/pairmatching/enums/Menu.java
@@ -0,0 +1,24 @@
+package pairmatching.enums;
+
+public enum Menu {
+    MATCHING("1", "페어 매칭"),
+    SEARCH("2", "페어 조회"),
+    INITIALIZE("3", "페어 초기화"),
+    QUIT("Q", "종료");
+
+    private final String menuNumber;
+    private final String menuName;
+
+    Menu(String menuNumber, String menuName) {
+        this.menuNumber = menuNumber;
+        this.menuName = menuName;
+    }
+
+    public String getMenuNumber() {
+        return menuNumber;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+}
