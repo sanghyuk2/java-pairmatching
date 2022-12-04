7d39586909b86fe06ff48763d17fa0a2d49af40d
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-04 14:20:49 +0900

    feat: 메뉴 선택 기능 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,10 +1,19 @@
 package pairmatching.view;
 
+import pairmatching.util.ValidateMenuNumber;
 import pairmatching.util.Validator;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    public static String readMenuNumber() {
+        return validateInput("기능을 선택하세요.\n" +
+                "1. 페어 매칭\n" +
+                "2. 페어 조회\n" +
+                "3. 페어 초기화\n" +
+                "Q. 종료", new ValidateMenuNumber());
+    }
+
     private static String validateInput(String message, Validator validate) {
         System.out.println(message);
         while (true) {
