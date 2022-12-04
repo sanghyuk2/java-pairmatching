128aa18c8e0e01f82c3c55f685025f18cde485ef
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-04 14:18:04 +0900

    feat: 입력값이 유효한 메뉴 번호인지 검증하는 클래스 및 메소드 구현

---
+++ src/main/java/pairmatching/util/ValidateMenuNumber.java
@@ -0,0 +1,11 @@
+package pairmatching.util;
+
+import pairmatching.domain.enums.Menu;
+
+public class ValidateMenuNumber implements Validator {
+
+    @Override
+    public void validate(String input) {
+        Menu.findMenu(input);
+    }
+}
