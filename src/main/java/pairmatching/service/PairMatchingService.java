a5ca8827f6fef6dbd6a42b8cc01a51bb082eb577
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-04 17:17:59 +0900

    feat: 이미 저장된 매치인지 확인하는 메소드 구현

--- src/main/java/pairmatching/service/PairMatchingService.java
+++ src/main/java/pairmatching/service/PairMatchingService.java
@@ -1,11 +1,17 @@
 package pairmatching.service;
 
+import pairmatching.domain.Stage;
+import pairmatching.domain.enums.Course;
+import pairmatching.domain.enums.Level;
 import pairmatching.repository.PairMatchingRepository;
 
 public class PairMatchingService {
     private final PairMatchingRepository pairMatchingRepository = new PairMatchingRepository();
+    Stage stage;
 
-    public boolean isAlreadyMatched(String readStage) {
-        return true;
+    public boolean isAlreadyMatched(String input) {
+        String[] stageArr = input.split(", ");
+        stage = new Stage(Course.findCourse(stageArr[0]), Level.findLevel(stageArr[1]), stageArr[2]);
+        return pairMatchingRepository.hasPair(stage);
     }
 }
